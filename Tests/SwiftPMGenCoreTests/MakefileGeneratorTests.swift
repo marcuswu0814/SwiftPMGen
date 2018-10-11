16bb1dede1f15eaae9d6c4bb473ca96c26bbd8c0
Marcus Wu <marcus.wu0814@gmail.com>
2018-10-11 21:49:14 +0800

    Step 4. Writing test

---
+++ Tests/SwiftPMGenCoreTests/MakefileGeneratorTests.swift
@@ -0,0 +1,53 @@
+import XCTest
+@testable import SwiftPMGenCore
+
+final class MakefileGeneratorTests: XCTestCase {
+    
+    func test__givenProdName__returnMakefileContent() {
+        let sut = MakefileGenerator()
+        
+        let result = sut.content(withProdName: "TestProd",
+                                 prodNameForHomebrew: "test-prod")
+        
+        XCTAssertEqual(result, exceptResult)
+    }
+    
+    private var exceptResult: String {
+        return """
+        PREFIX?=/usr/local
+
+        PROD_NAME=TestProd
+        PROD_NAME_HOMEBREW=test-prod
+
+        build:
+            swift build --disable-sandbox -c release -Xswiftc -static-stdlib
+
+        build-for-linux:
+            swift build --disable-sandbox -c release
+
+        run:
+            .build/release/$(PROD_NAME)
+
+        test: xcode
+            set -o pipefail && xcodebuild -scheme "${PROD_NAME}-Package" -enableCodeCoverage YES clean build test | xcpretty
+
+        lint:
+            swiftlint
+
+        update:
+            swift package update
+
+        clean:
+            swift package clean
+
+        xcode:
+            swift package generate-xcodeproj
+
+        install: build
+            mkdir -p "$(PREFIX)/bin"
+            cp -f ".build/release/$(PROD_NAME)" "$(PREFIX)/bin/$(PROD_NAME_HOMEBREW)"
+        """
+    }
+    
+}
+
