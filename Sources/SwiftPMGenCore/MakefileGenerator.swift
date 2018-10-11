d8594010426542e648b4f1cf430e5b73780c4919
Marcus Wu <marcus.wu0814@gmail.com>
2018-10-11 21:50:11 +0800

    Step 5. Implement MakefileGenerator and pass test

---
+++ Sources/SwiftPMGenCore/MakefileGenerator.swift
@@ -0,0 +1,40 @@
+class MakefileGenerator {
+    
+    func content(withProdName prodName: String, prodNameForHomebrew: String) -> String {
+        return """
+        PREFIX?=/usr/local
+        
+        PROD_NAME=\(prodName)
+        PROD_NAME_HOMEBREW=\(prodNameForHomebrew)
+        
+        build:
+            swift build --disable-sandbox -c release -Xswiftc -static-stdlib
+        
+        build-for-linux:
+            swift build --disable-sandbox -c release
+        
+        run:
+            .build/release/$(PROD_NAME)
+        
+        test: xcode
+            set -o pipefail && xcodebuild -scheme "${PROD_NAME}-Package" -enableCodeCoverage YES clean build test | xcpretty
+        
+        lint:
+            swiftlint
+        
+        update:
+            swift package update
+        
+        clean:
+            swift package clean
+        
+        xcode:
+            swift package generate-xcodeproj
+        
+        install: build
+            mkdir -p "$(PREFIX)/bin"
+            cp -f ".build/release/$(PROD_NAME)" "$(PREFIX)/bin/$(PROD_NAME_HOMEBREW)"
+        """
+    }
+    
+}
